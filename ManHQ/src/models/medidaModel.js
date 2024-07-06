4fe44731a80fb33e4fd428d0b9733a6ffff0a9a9
GabrielCordeiro2006 <gabriel.silva877@sptech.school>
2024-07-05 23:15:46 -0300

    atualização back end

--- ManHQ/src/models/medidaModel.js
+++ ManHQ/src/models/medidaModel.js
@@ -30,8 +30,32 @@ function buscarUltimasMedidas() {
     `;
     return database.executar(instrucaoSql);
   }
+
+  function buscarMedidasEmTempoReal_l() {
+    const instrucaoSql = `
+  // -- Selecionar o usuário com a maior quantidade de leitura
+SELECT * FROM dados 
+WHERE leitura = (SELECT MAX(leitura) FROM dados);
+    `;
+    return database.executar(instrucaoSql);
+  }
+
+
+  function buscarMedidasEmTempoReal_t() {
+    const instrucaoSql = `
+// -- Selecionar o usuário com a menor quantidade de leitura
+SELECT * FROM dados 
+WHERE leitura = (SELECT MIN(leitura) FROM dados);
+    `;
+    return database.executar(instrucaoSql);
+  }
+
+
+
   
   module.exports = {
     buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
+    buscarMedidasEmTempoReal,
+    buscarMedidasEmTempoReal_l,
+    buscarMedidasEmTempoReal_t   
   };
